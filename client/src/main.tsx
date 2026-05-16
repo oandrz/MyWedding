a38a9301da3fadab587e76be9629c676d6f857fd
andreas.oentoro <oentoro.andreas@gmail.com>
2026-05-16 15:54:57 +0800

    feat: capture invite code to sessionStorage before React renders

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const _code = new URLSearchParams(window.location.search).get('code');
+if (_code) sessionStorage.setItem('inviteCode', _code);
+
 createRoot(document.getElementById("root")!).render(<App />);
