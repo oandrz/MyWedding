b52d32fbc86eb249c8121163493e8a3a37388087
andreas.oentoro <oentoro.andreas@gmail.com>
2026-05-16 15:55:37 +0800

    fix: rename _code to inviteCode in main.tsx for clarity

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const _code = new URLSearchParams(window.location.search).get('code');
-if (_code) sessionStorage.setItem('inviteCode', _code);
+const inviteCode = new URLSearchParams(window.location.search).get('code');
+if (inviteCode) sessionStorage.setItem('inviteCode', inviteCode);
 
 createRoot(document.getElementById("root")!).render(<App />);
