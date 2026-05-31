1989d3d6f30f516c5b7eaf6cb40a469ddc4ebefc
Andreas Oentoro <blaze796@gmail.com>
2026-05-31 18:16:24 +0800

    feat: remove Google Drive setup/memories pages and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,6 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import Home from "@/pages/Home";
 import Gallery from "@/pages/Gallery";
-import MemoriesGoogleDrive from "@/pages/MemoriesGoogleDrive";
-import MemoriesGoogleDriveUpload from "@/pages/MemoriesGoogleDriveUpload";
-import GoogleDriveSetup from "@/pages/GoogleDriveSetup";
-import GoogleDriveInstructions from "@/pages/GoogleDriveInstructions";
 import { AdminLayout } from "@/pages/admin/AdminLayout";
 import AdminLogin from "@/pages/AdminLogin";
 import NotFound from "@/pages/not-found";
@@ -23,10 +19,6 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/gallery" component={Gallery} />
       <Route path="/memories" component={Gallery} />
-      <Route path="/memories-drive" component={MemoriesGoogleDrive} />
-      <Route path="/memories-upload" component={MemoriesGoogleDriveUpload} />
-      <Route path="/google-drive-setup" component={GoogleDriveSetup} />
-      <Route path="/google-drive-instructions" component={GoogleDriveInstructions} />
       <Route path="/admin" nest>
         <AdminLayout />
       </Route>
